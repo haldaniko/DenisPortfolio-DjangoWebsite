373ab7d23860641bd7532cc30900b4a5dd71fc5b
haldaniko <haldaniko@gmail.com>
2025-09-19 16:10:26 +0300

    add multilanguage support

--- website/urls.py
+++ website/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("ua/", views.index, {'lang': 'ua'}, name="index_ua"),
+    path("bg/", views.index, {'lang': 'bg'}, name="index_bg"),
+    path("en/", views.index, {'lang': 'en'}, name="index_en"),
 ]
+
+app_name = "website"
--- website/views.py
+++ website/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from .models import Case, Testimony
 
+LANG_TEMPLATES = {
+    'ru': 'index.html',
+    'ua': 'index_ua.html',
+    'bg': 'index_bg.html',
+    'en': 'index_en.html',
+}
 
-def index(request):
-    cases = Case.objects.all()
-    testimonies = Testimony.objects.all()
+
+def index(request, lang='ru'):
+    template = LANG_TEMPLATES.get(lang, 'index.html')
     context = {
-        'cases': cases,
-        'testimonies': testimonies
+        'cases': Case.objects.all(),
+        'testimonies': Testimony.objects.all()
     }
-    return render(request, "index.html", context)
+    return render(request, template, context)
